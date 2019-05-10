3a314094e08cb040db7ad9ef0951d4cb796efa2d
Sebastian Schulz <sebastian.schulz@plath.de>
2019-05-10 14:11:55 +0200

    Webspell-RM/Webspell-RM-2.0#20 - Delete dublicated file bbcode.js and fix link in admincenter

--- admin/admincenter.php
+++ admin/admincenter.php
@@ -448,7 +448,7 @@ if ($getavatar = getavatar($userID)) {
     <script>
         var calledfrom='admin';
     </script>
-    <script src="../components/admin/js/bbcode.js"></script>
+    <script src="../components/js/bbcode.js"></script>
     <script>
     $(document).ready(function () {
 
