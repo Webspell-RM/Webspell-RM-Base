a7e1f60d010456ec7408485e24164213f8baa9fc
T-Seven <t-seven@die-irre-truppe.de>
2019-10-13 19:42:01 +0200

    admincenter3

--- admin/admincenter.php
+++ admin/admincenter.php
@@ -136,7 +136,7 @@ if ($getavatar = getavatar($userID)) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Hello, world!</title>
+    <title>Webspell | RM - Bootstrap Admin Theme</title>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
