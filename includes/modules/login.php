fef0b8b51bdc9305301ff68090de9d967fb6d08f
blubber86 <50556232+blubber86@users.noreply.github.com>
2019-10-19 20:14:36 +0200

    Update login.php

--- includes/modules/login.php
+++ includes/modules/login.php
@@ -27,99 +27,347 @@
 |           For Support, Mods and the Full Script visit             |
 |                       webspell-rm.de                              |
 \__________________________________________________________________*/
-$_language->readModule('login');
 
-if ($loggedin && $cookievalue == 'accepted') {
-
-    $_language->readModule('loginoverview', true);
-
-    if ($userID && !isset($_GET[ 'userID' ]) && !isset($_POST[ 'userID' ])) {
-
-        $data_array = array();
-        $data_array['$title'] = $_language->module[ 'overview' ];
-        $template = $tpl->loadTemplate("loginoverview", "head", $data_array);
-        echo $template;
-
-        $ds = mysqli_fetch_array(
-            safe_query(
-                "SELECT
-                        `registerdate`
-                    FROM `" . PREFIX . "user`
-                    WHERE `userID` = " . $userID
-            )
-        );
-
-        $username = '<a class="btn btn-info" href="index.php?site=profile&amp;id=' . $userID . '"><i class="fa fa-user"></i> ' . getnickname($userID) . '</a>';
-        $lastlogin = getformatdatetime($_SESSION[ 'ws_lastlogin' ]);
-        $registerdate = getformatdatetime($ds[ 'registerdate' ]);
-
-        //clanmember/admin/referer
-
-        if (isanyadmin($userID)) {
-            $admincenterpic =
-                '<a class="btn btn-success btn-lg" href="admin/admincenter.php" target="_blank">
-                    <i class="fas fa-cogs"></i> '.$_language->module[ 'admin' ].'</a>';
-        } else {
-            $admincenterpic = '';
-        }
-
-        if (isset($_SESSION[ 'referer' ])) {
-            $referer_uri = '<a class="btn" href="' . $_SESSION[ 'referer' ] . '">
-                <i class="fas fa-chevron-left"></i> ' .
-                $_language->module[ 'back_last_page' ] . '</a>';
-            unset($_SESSION[ 'referer' ]);
-        } else {
-            $referer_uri = '';
-        }
-
-        $data_array = array();
-        $data_array['$_modulepath'] = MODULE;
-        $data_array['$username'] = $username;
-        $data_array['$lastlogin'] = $lastlogin;
-        $data_array['$registerdate'] = $registerdate;
-        $data_array['$referer_uri'] = $referer_uri;
-        $data_array['$admincenterpic'] = $admincenterpic;
-
-        $data_array['$buddy_list'] = $_language->module[ 'buddy_list' ];
-        $data_array['$messenger'] = $_language->module[ 'messenger' ];
-        $data_array['$edit_account'] = $_language->module[ 'edit_account' ];
-        $data_array['$logout'] = $_language->module[ 'logout' ];
-        $data_array['$user'] = $_language->module[ 'user' ];
-        $data_array['$last_login'] = $_language->module[ 'last_login' ];
-        $data_array['$registered'] = $_language->module[ 'registered' ];
-        $data_array['$informations'] = $_language->module[ 'informations' ];
-        $data_array['$menu'] = $_language->module[ 'menu' ];
-
-        $template = $tpl->loadTemplate("loginoverview", "content", $data_array);
-        echo $template;
-
-    } else {
-        echo $_language->module[ 'you_have_to_be_logged_in' ];
+// by ZENITH-Developments.de # read database entries (?)
+$_admin_minpasslen = "6";
+$_admin_maxpasslen = ""; #empty = no max
+$_admin_musthavelow = true;
+$_admin_musthaveupp = true;
+$_admin_musthavenum = true;
+$_admin_musthavespec = true;
+function pass_complex($pwd,$_admin_minpasslen,$_admin_maxpasslen,$_admin_musthavelow,$_admin_musthaveupp,$_admin_musthavenum,$_admin_musthavespec) {
+    if ($_admin_musthavelow==true) { $_pwd_low = "(?=\S*[a-z])"; } else { $_pwd_low=""; }
+    if ($_admin_musthaveupp==true) { $_pwd_upp = "(?=\S*[A-Z])"; } else { $_pwd_upp=""; }
+    if ($_admin_musthavenum==true) { $_pwd_num = "(?=\S*[\d])"; } else { $_pwd_num=""; }
+    if ($_admin_musthavespec==true) { $_pwd_spec = "(?=\S*[\W])"; } else { $_pwd_spec=""; }
+    if (!preg_match_all('$\S*(?=\S{'.$_admin_minpasslen.','.$_admin_maxpasslen.'})'.$_pwd_low.$_pwd_upp.$_pwd_num.$_pwd_spec.'\S*$', $pwd)) {
+        return false;
     }
+    return true;
+}
 
-} else {
+try {
+    $get = mysqli_fetch_assoc(safe_query("SELECT * FROM `".PREFIX."settings_recaptcha`"));
+    $webkey = $get['webkey'];
+    $seckey = $get['seckey'];
+    if ($get['activated']=="1") { $recaptcha=1; } else { $recaptcha=0; }
+} Catch (EXCEPTION $e) {
+    $recaptcha=0;
+}
 
-    //set sessiontest variable (checks if session works correctly)
-    $_SESSION[ 'ws_sessiontest' ] = true;
+$_language->readModule('register');
 
-    $data_array=array();
-    $data_array['$_modulepath'] = substr(MODULE, 0, -1);
-    $data_array['$login_titel'] = $_language->module[ 'login_titel' ];
-    $data_array['$login'] = $_language->module[ 'login' ];
-    $data_array['$lang_register'] = $_language->module[ 'register' ];
-    $data_array['$info'] = $_language->module[ 'info' ];
-    $data_array['$info1'] = $_language->module[ 'info1' ];
-    $data_array['$info2'] = $_language->module[ 'info2' ];
-    $data_array['$info3'] = $_language->module[ 'info3' ];
-    $data_array['$info4'] = $_language->module[ 'info4' ];
-    $data_array['$register_now'] = $_language->module[ 'register_now' ];
-    $data_array['$lost_password'] = $_language->module[ 'lost_password' ];
-    
-    if($cookievalue == 'accepted') {
-        $loginform = $tpl->loadTemplate("login", "content", $data_array);
-        echo $loginform;
+
+    $data_array = array();
+    $data_array['$title'] = $_language->module[ 'title' ];
+    $template = $tpl->loadTemplate("register","head", $data_array);
+    echo $template;
+
+$show = true;
+if (isset($_POST['save'])) {
+    if (!$loggedin) {
+        $nickname = htmlspecialchars(mb_substr(trim($_POST['nickname']), 0, 30));
+        if (strpos($nickname, "'") !== false) {
+            $nickname = "";     // contains a ' char the nickname will reset (handle as not entered)
+        }
+        $password = $_POST['password'];
+
+        $mail = $_POST['mail'];
+        $CAPCLASS = new \webspell\Captcha;
+
+        $error = array();
+        
+        // check nickname
+        if (!(mb_strlen(trim($nickname)))) {
+            $error[] = $_language->module['enter_nickname'];
+        }
+
+        // check nickname inuse
+        $ergebnis = safe_query("SELECT * FROM " . PREFIX . "user WHERE nickname = '$nickname' ");
+        $num = mysqli_num_rows($ergebnis);
+        if ($num) {
+            $error[] = $_language->module['nickname_inuse'];
+        }
+
+        
+        // check passwort
+        if (pass_complex($password,$_admin_minpasslen,$_admin_maxpasslen,$_admin_musthavelow,$_admin_musthaveupp,$_admin_musthavenum,$_admin_musthavespec)==false) {
+            $error[] = $_language->module['enter_password2'];
+        }
+
+        // check e-mail
+        if (!validate_email($mail)) {
+            $error[] = $_language->module['invalid_mail'];
+        }
+
+        // check e-mail inuse
+        $ergebnis = safe_query("SELECT userID FROM " . PREFIX . "user WHERE email = '$mail' ");
+        $num = mysqli_num_rows($ergebnis);
+        if ($num) {
+            $error[] = $_language->module['mail_inuse'];
+        }
+
+        // check captcha
+        if($recaptcha=="0") { 
+            if (!$CAPCLASS->checkCaptcha($_POST['captcha'], $_POST['captcha_hash'])) {
+                $error[] = "Securitycode Error";
+            } else { $runregister = "false"; }
+        } else {
+      
+                  $msg='';
+            if($_SERVER["REQUEST_METHOD"] == "POST") {
+                $recaptcha=$_POST['g-recaptcha-response'];
+                if(!empty($recaptcha)) {
+                    include("system/curl_recaptcha.php");
+                    $google_url="https://www.google.com/recaptcha/api/siteverify";
+                    $secret=$seckey;
+                    $ip=$_SERVER['REMOTE_ADDR'];
+                    $url=$google_url."?secret=".$secret."&response=".$recaptcha."&remoteip=".$ip;
+                    $res=getCurlData($url);
+                    $res= json_decode($res, true);
+                    $runregister = "true";
+                    //reCaptcha success check 
+                    if(!$res['success']) {
+                        $error[] = "reCAPTCHA Error";
+                        $runregister = "false";
+                    }
+                } else {
+                    $error[] = "reCAPTCHA Error";
+                    $runregister = "false";
+                }
+            }
+        }
+        
+        // check exisitings accounts from ip with same password
+        if(!$register_per_ip) {
+            $get_users =
+                safe_query(
+                    "SELECT
+                        userID
+                    FROM
+                        " . PREFIX . "user
+                    WHERE
+                        ip='" . $GLOBALS['ip'] . "'"
+                );
+            if (mysqli_num_rows($get_users)) {
+                $error[] = 'Only one Account per IP';
+            }
+        }
+
+        if (count($error)) {
+            $_language->readModule('formvalidation', true);
+            $showerror = generateErrorBoxFromArray($_language->module['errors_there'], $error);
+        } else {
+            // insert in db
+            $registerdate = time();
+            $activationkey = md5(RandPass(20));
+            $activationlink = getCurrentUrl() . '&key=' . $activationkey;
+
+            safe_query(
+                "INSERT INTO
+                    `" . PREFIX . "user` (
+                        `registerdate`,
+                        `lastlogin`,
+                        `nickname`,
+                        `email`,
+                        `newsletter`,
+                        `activated`,
+                        `ip`,
+                        `date_format`,
+                        `time_format`
+                    )
+                    VALUES (
+                        '$registerdate',
+                        '$registerdate',
+                        '$nickname',
+                        '$mail',
+                        '0',
+                        '" . $activationkey . "',
+                        '" . $GLOBALS['ip'] . "',
+                        '" . $default_format_date . "',
+                        '" . $default_format_time . "'
+                    )"
+            );
+
+            $insertid = mysqli_insert_id($_database);
+            
+            // insert Password
+            $pass = Gen_PasswordHash(stripslashes($password), $insertid);
+            safe_query("UPDATE `".PREFIX."user` SET `password_hash` = '".$pass."' WHERE `userID` = '".intval($insertid)."'");
+
+            // insert in user_groups
+            safe_query("INSERT INTO " . PREFIX . "user_groups ( userID ) values('$insertid' )");
+
+            // mail to user
+            $ToEmail = $mail;
+            $header = str_replace(
+                array('%nickname%', '%activationlink%', '%pagetitle%', '%homepage_url%'),
+                array(stripslashes($nickname), stripslashes($activationlink), $hp_title, $hp_url),
+                $_language->module['mail_subject']
+            );
+            $Message = str_replace(
+                array('%nickname%', '%activationlink%', '%pagetitle%', '%homepage_url%'),
+                array(stripslashes($nickname), stripslashes($activationlink), $hp_title, $hp_url),
+                $_language->module['mail_text']
+            );
+            $sendmail = \webspell\Email::sendEmail($admin_email, 'Register', $ToEmail, $header, $Message);
+
+            if ($sendmail['result'] == 'fail') {
+                if (isset($sendmail['debug'])) {
+                    $fehler = array();
+                    $fehler[] = $sendmail[ 'error' ];
+                    $fehler[] = $sendmail[ 'debug' ];
+                    redirect(
+                        "index.php",
+                        generateErrorBoxFromArray($_language->module['mail_failed'], $fehler),
+                        10
+                    );
+                    $show = false;
+                } else {
+                    $fehler = array();
+                    $fehler[] = $sendmail['error'];
+                    redirect(
+                        "index.php",
+                        generateErrorBoxFromArray($_language->module['mail_failed'], $fehler),
+                        10
+                    );
+                    $show = false;
+                }
+            } else {
+                if (isset($sendmail['debug'])) {
+                    $fehler = array();
+                    $fehler[] = $sendmail[ 'debug' ];
+                    redirect(
+                        "index.php",
+                        generateBoxFromArray($_language->module['register_successful'], 'alert-success', $fehler),
+                        10
+                    );
+                    $show = false;
+                } else {
+                    redirect("index.php", $_language->module['register_successful'], 3);
+                    $show = false;
+                }
+            }
+        }
     } else {
-        $loginform = $tpl->loadTemplate("login", "cookie_error", $data_array);
-        echo $loginform;
+        redirect(
+            "index.php?site=register",
+            str_replace('%pagename%', $GLOBALS['hp_title'], $_language->module['no_register_when_loggedin']),
+            3
+        );
+    }
+}
+if (isset($_GET['key'])) {
+    safe_query("UPDATE `" . PREFIX . "user` SET activated='1' WHERE activated='" . $_GET['key'] . "'");
+    if (mysqli_affected_rows($_database)) {
+        redirect('index.php?site=login', $_language->module['activation_successful'], 3);
+    } else {
+        redirect('index.php?site=login', $_language->module['wrong_activationkey'], 3);
+    }
+} elseif (isset($_GET['mailkey'])) {
+    if (mb_strlen(trim($_GET['mailkey'])) == 32) {
+        safe_query(
+            "UPDATE
+                `" . PREFIX . "user`
+            SET
+                email_activate='1',
+                email=email_change,
+                email_change=''
+            WHERE
+                email_activate='" . $_GET['mailkey'] . "'"
+        );
+        if (mysqli_affected_rows($_database)) {
+            redirect('index.php?site=login', $_language->module['mail_activation_successful'], 3);
+        } else {
+            redirect('index.php?site=login', $_language->module['wrong_activationkey'], 3);
+        }
+    }
+} else {
+    if ($show === true) {
+        if (!$loggedin) {
+            if ($cookievalue == 'accepted') {
+                if($recaptcha=="0") {
+                    $CAPCLASS = new \webspell\Captcha;
+                    $captcha = $CAPCLASS->createCaptcha();
+                    $hash = $CAPCLASS->getHash();
+                    $CAPCLASS->clearOldCaptcha();
+                    $_captcha = '
+                        <span class="input-group-addon captcha-img">'.$captcha.'</span>
+                        <input type="number" name="captcha" class="form-control" id="input-security-code">
+                        <input name="captcha_hash" type="hidden" value="'.$hash.'">
+                    ';
+                } else {
+                    $_captcha = '<div class="g-recaptcha" style="width: 70%; float: left;" data-sitekey="'.$webkey.'"></div>';
+                }
+                if (!isset($showerror)) {
+                    $showerror = '';
+                }
+                if (isset($_POST['nickname'])) {
+                    $nickname = getforminput($_POST['nickname']);
+                } else {
+                    $nickname = '';
+                }
+                if (isset($_POST['password'])) {
+                    $password = getforminput($_POST['password']);
+                } else {
+                    $password = '';
+                }
+                if (isset($_POST['mail'])) {
+                    $mail = getforminput($_POST['mail']);
+                } else {
+                    $mail = '';
+                }
+
+                $data_array = array();
+                $data_array['$showerror'] = $showerror;
+                $data_array['$nickname'] = $nickname;
+                $data_array['$password'] = $password;
+                $data_array['$mail'] = $mail;
+                $data_array['$_captcha'] = $_captcha;
+                $data_array['$registration'] = $_language->module[ 'registration' ];
+                $data_array['$info'] = $_language->module[ 'info' ];
+                $data_array['$nickname'] = $_language->module[ 'nickname' ];
+                $data_array['$for_login'] = $_language->module[ 'for_login' ];
+                $data_array['$password'] = $_language->module[ 'password' ];
+                $data_array['$mail'] = $_language->module[ 'mail' ];
+                $data_array['$security_code'] = $_language->module[ 'security_code' ];
+                $data_array['$register_now'] = $_language->module[ 'register_now' ];
+                $data_array['$profile_info'] = $_language->module[ 'profile_info' ];
+                $data_array['$pass_ver'] = $_language->module[ 'pass_ver' ];
+                $data_array['$pass_text'] = $_language->module[ 'pass_text' ];
+                $data_array['$lang_GDPRinfo'] = $_language->module['GDPRinfo'];
+                $data_array['$lang_GDPRaccept'] = $_language->module['GDPRaccept'];
+                $data_array['$lang_GDPRterm'] = $_language->module['GDPRterm'];
+                $data_array['$lang_privacy_policy'] = $_language->module['privacy_policy'];
+                $data_array['$pw1'] = $_language->module['pw1'];
+                $data_array['$pw2'] = $_language->module['pw2'];
+                $data_array['$pw3'] = $_language->module['pw3'];
+                $data_array['$pw4'] = $_language->module['pw4'];
+                $data_array['$pw5'] = $_language->module['pw5'];
+                $data_array['$pw6'] = $_language->module['pw6'];
+                $template = $tpl->loadTemplate("register","content", $data_array);
+                echo $template;
+            } else {
+                redirect(
+                    "index.php",
+                    str_replace(
+                        '%pagename%',
+                        $GLOBALS['hp_title'],
+                        $_language->module['no_cookie_accept']
+                    ),
+                    3
+                );
+            }
+        } else {
+            redirect(
+                "index.php",
+                str_replace(
+                    '%pagename%',
+                    $GLOBALS['hp_title'],
+                    $_language->module['no_register_when_loggedin']
+                ),
+                3
+            );
+        }
     }
 }
