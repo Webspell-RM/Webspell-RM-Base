8c4d4735f391d59b533060b191cc14a76670750e
T-Seven <t-seven@die-irre-truppe.de>
2019-10-19 21:35:31 +0200

    cookies

--- includes/modules/login.php
+++ includes/modules/login.php
@@ -28,18 +28,13 @@
 |                       webspell-rm.de                              |
 \__________________________________________________________________*/
 $_language->readModule('login');
-
 if ($loggedin && $cookievalue == 'accepted') {
-
     $_language->readModule('loginoverview', true);
-
     if ($userID && !isset($_GET[ 'userID' ]) && !isset($_POST[ 'userID' ])) {
-
         $data_array = array();
         $data_array['$title'] = $_language->module[ 'overview' ];
         $template = $tpl->loadTemplate("loginoverview", "head", $data_array);
         echo $template;
-
         $ds = mysqli_fetch_array(
             safe_query(
                 "SELECT
@@ -48,13 +43,10 @@ if ($loggedin && $cookievalue == 'accepted') {
                     WHERE `userID` = " . $userID
             )
         );
-
         $username = '<a class="btn btn-info" href="index.php?site=profile&amp;id=' . $userID . '"><i class="fa fa-user"></i> ' . getnickname($userID) . '</a>';
         $lastlogin = getformatdatetime($_SESSION[ 'ws_lastlogin' ]);
         $registerdate = getformatdatetime($ds[ 'registerdate' ]);
-
         //clanmember/admin/referer
-
         if (isanyadmin($userID)) {
             $admincenterpic =
                 '<a class="btn btn-success btn-lg" href="admin/admincenter.php" target="_blank">
@@ -62,7 +54,6 @@ if ($loggedin && $cookievalue == 'accepted') {
         } else {
             $admincenterpic = '';
         }
-
         if (isset($_SESSION[ 'referer' ])) {
             $referer_uri = '<a class="btn" href="' . $_SESSION[ 'referer' ] . '">
                 <i class="fas fa-chevron-left"></i> ' .
@@ -71,7 +62,6 @@ if ($loggedin && $cookievalue == 'accepted') {
         } else {
             $referer_uri = '';
         }
-
         $data_array = array();
         $data_array['$_modulepath'] = MODULE;
         $data_array['$username'] = $username;
@@ -79,7 +69,6 @@ if ($loggedin && $cookievalue == 'accepted') {
         $data_array['$registerdate'] = $registerdate;
         $data_array['$referer_uri'] = $referer_uri;
         $data_array['$admincenterpic'] = $admincenterpic;
-
         $data_array['$buddy_list'] = $_language->module[ 'buddy_list' ];
         $data_array['$messenger'] = $_language->module[ 'messenger' ];
         $data_array['$edit_account'] = $_language->module[ 'edit_account' ];
@@ -89,19 +78,14 @@ if ($loggedin && $cookievalue == 'accepted') {
         $data_array['$registered'] = $_language->module[ 'registered' ];
         $data_array['$informations'] = $_language->module[ 'informations' ];
         $data_array['$menu'] = $_language->module[ 'menu' ];
-
         $template = $tpl->loadTemplate("loginoverview", "content", $data_array);
         echo $template;
-
     } else {
         echo $_language->module[ 'you_have_to_be_logged_in' ];
     }
-
 } else {
-
     //set sessiontest variable (checks if session works correctly)
     $_SESSION[ 'ws_sessiontest' ] = true;
-
     $data_array=array();
     $data_array['$_modulepath'] = substr(MODULE, 0, -1);
     $data_array['$login_titel'] = $_language->module[ 'login_titel' ];
@@ -123,5 +107,4 @@ if ($loggedin && $cookievalue == 'accepted') {
         $loginform = $tpl->loadTemplate("login", "cookie_error", $data_array);
         echo $loginform;
     }
-
 }
