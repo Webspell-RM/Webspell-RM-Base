a3b9daee711894030830f73a3c6170082bec105d
blubber86 <50556232+blubber86@users.noreply.github.com>
2019-10-19 19:38:07 +0200

    Create cookie.php

---
+++ languages/en/cookie.php
@@ -0,0 +1,48 @@
+<?php
+/*¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\
+| _    _  ___  ___  ___  ___  ___  __    __      ___   __  __       |
+|( \/\/ )(  _)(  ,)/ __)(  ,\(  _)(  )  (  )    (  ,) (  \/  )      |
+| \    /  ) _) ) ,\\__ \ ) _/ ) _) )(__  )(__    )  \  )    (       |
+|  \/\/  (___)(___/(___/(_)  (___)(____)(____)  (_)\_)(_/\/\_)      |
+|                       ___          ___                            |
+|                      |__ \        / _ \                           |
+|                         ) |      | | | |                          |
+|                        / /       | | | |                          |
+|                       / /_   _   | |_| |                          |
+|                      |____| (_)   \___/                           |
+\___________________________________________________________________/
+/                                                                   \
+|        Copyright 2005-2018 by webspell.org / webspell.info        |
+|        Copyright 2018-2019 by webspell-rm.de                      |
+|                                                                   |
+|        - Script runs under the GNU GENERAL PUBLIC LICENCE         |
+|        - It's NOT allowed to remove this copyright-tag            |
+|        - http://www.fsf.org/licensing/licenses/gpl.html           |
+|                                                                   |
+|               Code based on WebSPELL Clanpackage                  |
+|                 (Michael Gruber - webspell.at)                    |
+\___________________________________________________________________/
+/                                                                   \
+|                     WEBSPELL RM Version 2.0                       |
+|           For Support, Mods and the Full Script visit             |
+|                       webspell-rm.de                              |
+\__________________________________________________________________*/
+
+$language_array = Array(
+
+/* do not edit above this line */
+
+  'accept'=>'Accept',
+  'privacy_policy'=>'Privacy Policy',
+  'txt'=>'This website uses cookies. If you use this website you agree to the use of cookies!',
+  'title'=>'Cookies',
+  'description' => 'sc_Datei Info',
+  'privacy_policy_title' => '<h4>Info for the manual integration of the sc_file. <br> <small> (Alternative: The widget control.) </ small></h4><br>',
+  'privacy_policy_text' => '<p>Copy the following lines and paste them in the desired location in index.php.</p>',
+  'info'=>'<h2>Cookies</h2>
+<p>Internet pages partly use so-called cookies. Cookies do not damage your computer and contain no viruses. Cookies serve to make our offer more user-friendly, effective and safer. Cookies are small text files which are stored on your computer and which get saved on your browser.</p>
+<p>Most of the cookies we use are so called "session cookies". They are automatically deleted after your visit. Other cookies remain on your device until you delete them. These cookies allow us to recognize your browser on the next visit.</p>
+<p>You can set your browser to inform you about setting cookies and to allow cookies only in individual cases, to exclude the acceptance of cookies for specific cases or deny generaly as also as the automatic deletion of cookies when the browser is closed. The functionality of this website may be restricted when cookies are deactivated.</p><br />
+<p><em>Source reference: <a href="https://www.e-recht24.de">e-recht24.de</a></em></p>'
+
+  );
