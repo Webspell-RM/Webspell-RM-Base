8bb444d3cf2b607c72f09cf21ea11fc57409c723
T-Seven <t-seven@die-irre-truppe.de>
2019-12-08 00:18:17 +0100

    extra

--- includes/modules/navigation_login.php
+++ includes/modules/navigation_login.php
@@ -30,60 +30,7 @@
 ?>
 <li class="nav-item underline dropdown">
      <?php if($loggedin) {
-        // BOARDS MIT KATEGORIE
-        $boards = safe_query(
-            "SELECT * FROM " . PREFIX . "plugins_forum_boards"
-        );
-        while ($db = mysqli_fetch_array($boards)) {
-            $q = safe_query(
-                "SELECT * FROM " . PREFIX .
-                "plugins_forum_topics"
-            );
-            while ($lp = mysqli_fetch_assoc($q)) {
-               
-                        if ($userID) {
-                            $board_topics[ ] = $lp[ 'topicID' ];
-                        } else {
-                            break;
-                        }
-            }
-
-            // get unviewed topics
-            $found = false;
-
-            if ($userID) {
-                $gv = mysqli_fetch_array(safe_query("SELECT topics FROM " . PREFIX . "user WHERE userID='$userID'"));
-                $array = explode("|", $gv[ 'topics' ]);
-
-                foreach ($array as $split) {
-                    if ($split != "" && in_array($split, $board_topics)) {
-                        $found = true;
-                        break;
-                    }
-                }
-            }
-
-
-            if ($found) {
-                $icon = '<span style="margin-top: -15px" class="badge badge-warning"><i class="fas fa-comments"></i></span>';
-            } else {
-                $icon = '';
-            }
-
-
-            $data_array = array();
-            $data_array['$icon'] = $icon;
-            
-       }
-
-  @$newmessages = getnewmessages($userID);
-  if ($newmessages == 1) {
-      $newmessages = $index_language[ 'one_new_message' ];
-    } elseif ($newmessages > 1) {
-      $newmessages = str_replace('%new_messages%', $newmessages, $index_language[ 'more_new_message' ]);
-     } else {
-      $newmessages = $index_language[ 'no_new_messages' ];
-    }
+     
         echo'<li class="nav-item dropdown mr-2">
             <a class="nav-link dropdown-toggle" href="#" id="dropdown09" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' . ucfirst($index_language[ 'overview' ]) . '</a>
             <div class="dropdown-menu subnav" aria-labelledby="dropdown09">
