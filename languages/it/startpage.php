1f375813fdecf1bb3a3672226779e8d95a0207f9
McRobert <ventura.robi@gmail.com>
2020-01-04 16:47:35 +0100

    It Language Update

--- languages/it/startpage.php
+++ languages/it/startpage.php
@@ -35,7 +35,6 @@ $language_array = array(
     'startpage' => 'Pagina Principale',
     'no_startpage' => 'Nessuna home page disponibile',
     'stand1'=>'<hr><b>Modifiche alle normative sulla protezione dei dati</b><br>',
-
-'stand2'=>' si riserva il diritto di modificare questa politica generale di protezione dei dati nel quadro delle normative sulla protezione dei dati applicabili.<br>Alzarsi:'
+    'stand2'=>' si riserva il diritto di modificare questa politica generale di protezione dei dati nel quadro delle normative sulla protezione dei dati applicabili.<br>'
 );
 
