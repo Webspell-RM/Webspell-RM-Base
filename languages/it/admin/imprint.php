778cabaf22dc11eb9a04a7e3399b7ebed0c7652a
T-Seven <t-seven@die-irre-truppe.de>
2020-01-04 18:00:10 +0100

    language it

---
+++ languages/it/admin/imprint.php
@@ -0,0 +1,45 @@
+<?php
+/*¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\
+| _    _  ___  ___  ___  ___  ___  __    __      ___   __  __       |
+|( \/\/ )(  _)(  ,)/ __)(  ,\(  _)(  )  (  )    (  ,) (  \/  )      |
+| \    /  ) _) ) ,\\__ \ ) _/ ) _) )(__  )(__    )  \  )    (       |
+|  \/\/  (___)(___/(___/(_)  (___)(____)(____)  (_)\_)(_/\/\_)      |
+|                       ___          ___                            |
+|                      |__ \        / _ \                           |
+|                         ) |      | | | |                          |
+|                        / /       | | | |                          |
+|                       / /_   _   | |_| |                          |
+|                      |____| (_)   \___/                           |
+\___________________________________________________________________/
+/                                                                   \
+|        Copyright 2005-2018 by webspell.org / webspell.info        |
+|        Copyright 2018-2019 by webspell-rm.de                      |
+|                                                                   |
+|        - Script runs under the GNU GENERAL PUBLIC LICENCE         |
+|        - It's NOT allowed to remove this copyright-tag            |
+|        - http://www.fsf.org/licensing/licenses/gpl.html           |
+|                                                                   |
+|               Code based on WebSPELL Clanpackage                  |
+|                 (Michael Gruber - webspell.at)                    |
+\___________________________________________________________________/
+/                                                                   \
+|                     WEBSPELL RM Version 2.0                       |
+|           For Support, Mods and the Full Script visit             |
+|                       webspell-rm.de                              |
+\__________________________________________________________________*/
+
+$language_array = Array(
+
+/* do not edit above this line */
+
+  'access_denied'=>'Accesso Negato',
+  'automatic'=>'Automatico',
+  'imprint'=>'Impronta Editoriale',
+  'title'=>'Impronta Editoriale',
+  'disclaimer' => 'Testo della dichiarazione di non responsabilità',
+  'manual'=>'Manuale (Usare la casella di testo inferiore)',
+  'transaction_invalid'=>'Transazione Invalida',
+  'update'=>'Aggiornamento',
+  'you_can_use_html'=>'È possibile utilizzare i tag HTML'
+  
+);
